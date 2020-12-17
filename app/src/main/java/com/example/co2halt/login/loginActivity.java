879416ffd2ed5b22c094f4cec6b27d030fdd9ec9
Sabari <r.sabarinathan22@gmail.com>
2020-12-17 16:50:01 +0530

    first commit

---
+++ app/src/main/java/com/example/co2halt/login/loginActivity.java
@@ -0,0 +1,16 @@
+package com.example.co2halt.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.co2halt.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
